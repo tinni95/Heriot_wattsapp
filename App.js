bddf04eb0d188c0da0deea163434cbf3d05c667d
Jordey92 <jordan@jordandey.com>
2019-08-03 18:29:49 +0100

    initial commit, loading expo from multiple apps

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>{Constants.manifest.extra.header}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Constants.manifest.primaryColor,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
